04cbf67c3068c9394f3ef698381d0d8b4f55b28c
shivanjalipabboju <shivanjalipabboju@gmail.com>
2026-07-18 22:42:59 +0530

    Create text to speech.py

---
+++ text to speech.py
@@ -0,0 +1,17 @@
+#text to speech
+# Text to speech
+from gtts import gTTS
+import gradio as gr
+
+def text_to_speech(text):
+  tts=gTTS(text=text,lang="en")
+  output_file="output.mp3"
+  tts.save(output_file)
+  return output_file
+
+demo=gr.Interface(fn=text_to_speech,
+                  inputs=gr.Textbox(lines=5,label="Enter Text"),
+                  outputs=gr.Audio(label="Speech"),
+                  title="Text-to-Speech"
+                  )
+demo.launch()
